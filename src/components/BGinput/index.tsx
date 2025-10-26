9af9d40496c5f3d6979105fef7391711b47210d1
HardBoris <b.gaibor03@gmail.com>
2025-10-26 10:15:23 -0300

    feat: cuestionario

---
+++ src/components/BGinput/index.tsx
@@ -0,0 +1,32 @@
+import "./input.css";
+import { InputHTMLAttributes } from "react";
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
+  register?: any;
+  name: string;
+  error?: any;
+  label?: string;
+}
+
+export const BGInput = ({
+  label,
+  register,
+  name,
+  placeholder,
+  error,
+  ...rest
+}: InputProps) => {
+  return (
+    <div className="input-form">
+      {label && <div className="input-label">{label}</div>}
+      <div className={!!error ? "input-field borded" : "input-field"}>
+        <input
+          {...(register && { ...register(name) })}
+          {...rest}
+          className={!!error ? "error" : ""}
+          placeholder={!!error ? `${error}` : `${placeholder}`}
+        />
+      </div>
+    </div>
+  );
+};
--- src/pages/Cuestionario/index.tsx
+++ src/pages/Cuestionario/index.tsx
@@ -1,9 +1,34 @@
+import { BGbutton } from "../../components/BGbutton";
 import { BGformulario } from "../../components/BGformulario";
+import { BGInput } from "../../components/BGinput";
+import "./cuestionario.css";
 
 export const Cuestionario = () => {
   return (
     <>
-      <BGformulario>Cuestionario</BGformulario>
+      <BGformulario clase="vertical-form">
+        <h1>Cuestionario</h1>
+        <div className="clasificacion">
+          <div className="start-separator">
+            <BGInput name="nivel" placeholder="nivel" />
+          </div>
+          <div className="separator">
+            <BGInput name="asunto" placeholder="asunto" />
+          </div>
+          <div className="end-separator">
+            <BGInput name="categoria" placeholder="categoria" />
+          </div>
+        </div>
+        <BGInput name="pregunta" placeholder="pregunta" />
+        <BGInput name="correcta" placeholder="correcta" />
+        <BGInput name="incorrecta1" placeholder="incorrecta1" />
+        <BGInput name="incorrecta2" placeholder="incorrecta2" />
+        <BGInput name="incorrecta3" placeholder="incorrecta3" />
+        <BGInput name="incorrecta4" placeholder="incorrecta4" />
+        <div className="action-form">
+          <BGbutton>Enviar</BGbutton>
+        </div>
+      </BGformulario>
     </>
   );
 };
