14f0a54565f7a454cd4a7e92b463791b1215f66d
HardBoris <b.gaibor03@gmail.com>
2025-12-25 20:07:43 -0300

    feat: cuestionario 769 view

--- src/pages/Cuestionario/index.tsx
+++ src/pages/Cuestionario/index.tsx
@@ -5,43 +5,50 @@ import "./cuestionario.css";
 
 export const Cuestionario = () => {
   return (
-    <div className="cuestionario">
-      <BGformulario clase="vertical-form">
-        <h1>Cuestionario</h1>
-        <div className="clasificacion">
-          <div className="start-separator">
-            <BGInput name="nivel" placeholder="nivel" />
-          </div>
-          <div className="separator">
-            <BGInput name="asunto" placeholder="asunto" />
-          </div>
-          <div className="end-separator">
-            <BGInput name="categoria" placeholder="categoria" />
-          </div>
-        </div>
-        <div className="separator">
-          <BGInput name="pregunta" placeholder="pregunta" />
-        </div>
-        <div className="separator">
-          <BGInput name="correcta" placeholder="correcta" />
-        </div>
-        <div className="separator">
-          <BGInput name="incorrecta1" placeholder="incorrecta1" />
-        </div>
-        <div className="separator">
-          <BGInput name="incorrecta2" placeholder="incorrecta2" />
-        </div>
-        <div className="separator">
-          <BGInput name="incorrecta3" placeholder="incorrecta3" />
-        </div>
-        <div className="separator">
-          <BGInput name="incorrecta4" placeholder="incorrecta4" />
-        </div>
+    <>
+      <header></header>
+      <main>
+        <div className="cuestionario">
+          <BGformulario clase="vertical-form">
+            <h1>Cuestionario</h1>
+            <div className="clasificacion">
+              <div className="start-separator">
+                <BGInput name="nivel" placeholder="nivel" />
+              </div>
+              <div className="separator">
+                <BGInput name="asunto" placeholder="asunto" />
+              </div>
+              <div className="end-separator">
+                <BGInput name="categoria" placeholder="categoria" />
+              </div>
+            </div>
+            <div className="separator">
+              <BGInput name="pregunta" placeholder="pregunta" />
+            </div>
+            <div className="separator">
+              <BGInput name="correcta" placeholder="correcta" />
+            </div>
+            <div className="separator">
+              <BGInput name="incorrecta1" placeholder="incorrecta1" />
+            </div>
+            <div className="separator">
+              <BGInput name="incorrecta2" placeholder="incorrecta2" />
+            </div>
+            <div className="separator">
+              <BGInput name="incorrecta3" placeholder="incorrecta3" />
+            </div>
+            <div className="separator">
+              <BGInput name="incorrecta4" placeholder="incorrecta4" />
+            </div>
 
-        <div className="separator">
-          <BGbutton>Guardar</BGbutton>
+            <div className="separator">
+              <BGbutton>Guardar</BGbutton>
+            </div>
+          </BGformulario>
         </div>
-      </BGformulario>
-    </div>
+      </main>
+
+      <footer></footer>
+    </>
   );
 };
