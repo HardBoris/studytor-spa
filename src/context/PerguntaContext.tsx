60b8dd912ab3bdfde5ce2e504024d1418beb34ce
HardBoris <b.gaibor03@gmail.com>
2025-12-25 08:42:09 -0300

    feat: Pergunta Context

---
+++ src/context/PerguntaContext.tsx
@@ -0,0 +1,60 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { localApi as api } from "../services/api";
+
+interface PerguntaProviderProps {
+  children: ReactNode;
+}
+
+export interface Pergunta {
+  perguntaId?: string;
+  pergunta: string;
+}
+
+interface PerguntaContextData {
+  perguntas: Pergunta[];
+  PerguntasList: () => void;
+}
+
+export const PerguntaContext = createContext<PerguntaContextData>(
+  {} as PerguntaContextData
+);
+
+const usePergunta = () => useContext(PerguntaContext);
+
+const PerguntaProvider = ({ children }: PerguntaProviderProps) => {
+  const [perguntas, setPerguntas] = useState<Pergunta[]>([]);
+
+  const PerguntasList = async () => {
+    await api
+      .get("/studytor-api/perguntas")
+      .then((response) => {
+        setPerguntas(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    PerguntasList();
+  }, []);
+
+  return (
+    <PerguntaContext.Provider
+      value={{
+        perguntas,
+        PerguntasList,
+      }}
+    >
+      {children}
+    </PerguntaContext.Provider>
+  );
+};
+
+export { usePergunta, PerguntaProvider };
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -2,15 +2,18 @@ import { ReactNode } from "react";
 import { DisciplinaProvider } from "./DisciplinaContext";
 import { AssuntoProvider } from "./AssuntoContext";
 import { CategoriaProvider } from "./CategoriaContext";
+import { PerguntaProvider } from "./PerguntaContext";
 
 interface AppProviderProps {
   children: ReactNode;
 }
 
 export const AppProvider = ({ children }: AppProviderProps) => (
-  <CategoriaProvider>
-    <AssuntoProvider>
-      <DisciplinaProvider>{children}</DisciplinaProvider>
-    </AssuntoProvider>
-  </CategoriaProvider>
+  <PerguntaProvider>
+    <CategoriaProvider>
+      <AssuntoProvider>
+        <DisciplinaProvider>{children}</DisciplinaProvider>
+      </AssuntoProvider>
+    </CategoriaProvider>
+  </PerguntaProvider>
 );
