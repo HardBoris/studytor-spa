4b01c5ddfe9951aaf84e1a0d08677fce0e3bfd42
HardBoris <b.gaibor03@gmail.com>
2026-01-15 18:50:49 -0300

    feat: wire navigator

---
+++ src/components/BGnavigator/index.tsx
@@ -0,0 +1,79 @@
+import { Link } from "react-router-dom";
+import "./navigator.style.css";
+//import { useAuth } from "../../context/UserContext";
+import {
+  FaArrowAltCircleDown,
+  // FaArrowAltCircleLeft,
+  // FaArrowAltCircleRight,
+  FaHome,
+  FaShoppingCart,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import { AiFillDashboard } from "react-icons/ai";
+// import { MdRequestPage, MdViewList } from "react-icons/md";
+// import { GiOpenTreasureChest } from "react-icons/gi";
+import { BiLogIn } from "react-icons/bi";
+
+export const BGNavigator = () => {
+  //const { signOut /* company */ } = useAuth();
+
+  return (
+    <nav>
+      <div className="navigator">
+        <Link to="/">
+          <span>Home</span>
+          {/* <FaHome /> */}
+        </Link>
+      </div>
+      <div className="navigator">
+        <Link to="/login">
+          <span>LogIn</span>
+          {/*<BiLogIn/>*/}
+        </Link>
+      </div>
+      <div className="navigator">
+        <Link to="/signup">
+          <span>SignUp</span>
+          {/* <FaArrowAltCircleDown/> */}
+        </Link>
+      </div>
+      {/* {company && (
+        <> */}
+      <div className="navigator">
+        <Link to="/" onClick={() => {}}>
+          <span>Dashboard</span>
+          {/* <AiFillDashboard/> */}
+        </Link>
+      </div>
+      <div className="navigator">
+        <Link to={`/test/purchases`} onClick={() => {}}>
+          <span>Compras</span>
+          {/* <FaShoppingCart/> */}
+        </Link>
+      </div>
+      {/* <div className="navigator">
+        <Link to={`/${company.companyId}/entries`} onClick={() => {}}>
+          <GiOpenTreasureChest />
+        </Link>
+      </div> */}
+      {/* <div className="navigator">
+        <Link to={`/${company.companyId}/movements`} onClick={() => {}}>
+          <MdViewList />
+        </Link>
+      </div> */}
+      {/* <div className="navigator">
+        <Link to={`/${company.companyId}/requisitions`} onClick={() => {}}>
+          <MdRequestPage />
+        </Link>
+      </div> */}
+      {/* </>
+      )} */}
+      {/*<div className="navigator">
+        <Link to="/" onClick={() => signOut()}>
+          {/* <span>Salir</span>
+          <FaSignOutAlt />
+        </Link>
+      </div>*/}
+    </nav>
+  );
+};
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -2,6 +2,8 @@ import { Outlet, Route, Routes } from "react-router-dom";
 //import { Pruebas } from "../pages/Pruebas";
 import { PerguntaNova } from "../pages/PerguntaNova";
 import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+import { Signup } from "../pages/Signup";
 
 export const PublicRoutes = () => {
   return (
@@ -9,6 +11,8 @@ export const PublicRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/perguntanova" element={<PerguntaNova />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Outlet />
     </>
